72c0d056366f0d3f37264d890eddd8604c3a9c0f
bima rijaluddin <bimarijal@gmail.com>
2019-09-19 11:38:50 +0800

    Tugas 8 javascript
    Tugas dari niomic

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function object(){
+    var laptop = {
+        merk : "zyrex",
+        body : "full metal",
+        harga : 3000000,
+        tahun : 2017,
+    }
+    laptop.tahun = 2018;
+    console.log (laptop.tahun);
+}
+
+object()
